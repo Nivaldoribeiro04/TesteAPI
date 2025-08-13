772ced357dcef6bcf058caa75a4538419fd9a3e8
Nivaldo Ribeiro <nivaldo.ribeiro@moorebrasil.com.br>
2025-08-13 15:11:41 -0300

    atualização

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+
+from typing import Annotated
+
 
 
 app = FastAPI()
@@ -10,3 +13,12 @@ async def root():
 @app.get("/admin")
 async def admin():
     return{"Admin": "Tela admin"}
+
+
+# @app.post("/files/")
+# async def create_file(file: Annotated[bytes, File()]):
+#     return{"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"Tipo do arquivo": file.content_type}
